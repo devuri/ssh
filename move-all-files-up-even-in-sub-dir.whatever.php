1d44d24c22c931e490fb1a72f9939f222910d1ee
@devuri <devuri@users.noreply.github.com>
2017-11-18 18:51:34 -0800

    Create move-all-files-up-even-in-sub-dir.whatever.php

---
+++ move-all-files-up-even-in-sub-dir.whatever.php
@@ -0,0 +1,16 @@
+mv */*.* ../
+
+mv = move 
+*/ = all sub directories
+
+*.* = any file type 
+
+variation 
+# move all mp3 file in dir up one level
+# dir structure = myalbumsfolder/myjay-z-albumfolder/songs.mp3
+
+mv */*.mp3 ../
+
+# asuming im in the myalbumsfolder/
+
+# all mp3 files will now be in the myalbumsfolder/ 
